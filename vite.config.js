3c8b1de2bb54998bd069ae02b02abe0894f96299
Phill Maslo <phillmaslo@gmail.com>
2025-02-09 19:34:00 +0100

    deleted unneeded files

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ command }) => {
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
-    base: "git@github.com:Phill1983/goit-js-hw-10.git",
+    base: "/goit-js-hw-10/",
     build: {
       sourcemap: true,
       rollupOptions: {
